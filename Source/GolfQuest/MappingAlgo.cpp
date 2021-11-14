f30befbddf33d2bf9c214c561a5289f29395b472
Streakman <46490210+MatthewLKomar@users.noreply.github.com>
2021-11-14 15:31:18 -0500

    fixed headerfile bug preventing it from compiling

--- Source/GolfQuest/MappingAlgo.cpp
+++ Source/GolfQuest/MappingAlgo.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "GenericPlatform/GenericPlatformMath.h"
 #include "MappingAlgo.h"
+#include "GenericPlatform/GenericPlatformMath.h"
 
 // Sets default values for this component's properties
 UMappingAlgo::UMappingAlgo()
